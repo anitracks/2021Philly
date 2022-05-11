8bc9dc2f8cfc0076f3b1886b5d722a299cd09ec5
Seth McNeill <seth@anitracks.com>
2022-05-11 05:56:45 -0700

    Added correlations.py documentation

--- src/correlations.py
+++ src/correlations.py
@@ -1,7 +1,8 @@
 """!
  @file correlations.py
- @brief Data analysis
- @details This finds some correlations between plaintiffs and outcomes
+ @brief Data analysis testing
+ @details This finds some correlations between plaintiffs and outcomes. This 
+ script was used for testing some ideas.
 
  @author Seth McNeill
  @date 2021 April 21
@@ -10,12 +11,7 @@
 import datetime  # used for start/end times
 import argparse  # Improved command line functionality
 import pdb       # Debugging
-import csv       # CSV file operations
 import pandas as pd  # Pandas data manipulation library
-import sqlite3   # built in sql database
-import numpy as np  # for data processing
-import re  # regular expression library for processing
-import math  # for isnan
 import cleanPlaintiffs  # to clean the plaintiff names
 import cleanCountOutcomes  # for cleaning df and outcomes
 
@@ -33,11 +29,7 @@ def main():
     # Plaintiff Attorney ID and Defendant Attorney ID come in as mixed types for some reason
     csvDF = pd.read_csv(args.csvfile)  # read in the csv data
 
-    # need to look at cases where the same attorney is listed on both sides
-    # try using sent2vec to sort out similar ones
-    # pypi.org/project/sent2vec
-
-    df = cleanCountOutcomes.cleanWholeDF(csvDF)
+    df = cleanCountOutcomes.removeDuplicates(csvDF)
     (plaintiffs, df) = cleanPlaintiffs.plaintiffList(df, 'Plaintiff Name(s)')
     (outcomes, df) = cleanCountOutcomes.outcomeList(df, 'Case Outcome')
     print(df['PlaintiffCleaned'].head())
@@ -50,13 +42,12 @@ def main():
     comparedPO = pd.crosstab(df['PlaintiffCleaned'], df['OutcomeCleaned'])  # Seems to be a good base dataset
     comparedPO.to_csv('comparedPO.csv')
     groupedPO = df.groupby(['PlaintiffCleaned','OutcomeCleaned']).size().reset_index().rename(columns={0:'count'})  # data not in right form
-    pdb.set_trace()
 
     # This is a bootstrapping method to find more of the similar plaintiff names
     matched = 0
     diffMatched = 0
     for index, value in plaintiffs.items():
-        nFind = len(cleanDF[cleanDF['Plaintiff Name(s)'].str.contains(index, case=False)])
+        nFind = len(df[df['Plaintiff Name(s)'].str.contains(index, case=False)])
         if nFind != value:
             print(f"{index} found {nFind} out of {value}")
             diffMatched += 1
